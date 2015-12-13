899eff62f4fd06fde7c6cf21263f46faca6595c2
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-12-13 20:27:13 +0000

    completed doctor model and contorller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,14 @@
 class DoctorsController < ApplicationController
   def index
+    @doctors = Doctor.all
   end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  private
+    def doctor_params
+      params.require(:doctor).permit(:name,:email,:phone,:address, :description)
+    end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,3 @@
 class Doctor < ActiveRecord::Base
+  belongs_to :address
 end
