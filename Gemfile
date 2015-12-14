c2b251883080aa6b526e2a0d63f0aa723059fde2
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-12-14 00:43:27 +0000

    added meta tag gem and function

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+# SEO
+gem 'meta-tags'
+
 #For associating files with db records
 gem "paperclip", "~> 4.1.1"
 gem 'rmagick'
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -5,10 +5,11 @@ class DoctorsController < ApplicationController
 
   def show
     @doctor = Doctor.find(params[:id])
+    set_meta_tags title: @doctor.name, description: @doctor.speciality+", "+@doctor.practice.name+", "+@doctor.practice.street+", "+@doctor.practice.city+", "+@doctor.practice.postal+", "+@doctor.description, keywords: 'Health, Doctors, Medicine, NHS, Telemedicine'
   end
 
   private
     def doctor_params
-      params.require(:doctor).permit(:name,:email,:phone,:address, :description)
+      params.require(:doctor).permit(:name,:email,:phone,:address, :description, :image)
     end
 end
